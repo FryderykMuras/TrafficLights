04ee4afeec03c8f406ed32e3a9bd0c560e9abbaf
lgosek <luki2012@gmail.com>
2019-01-16 14:04:05 +0100

    code cleanup

--- lights.erl
+++ lights.erl
@@ -1,18 +1,8 @@
 -module(lights).
 -compile(export_all).
-%-export([main/0, keyboardListener/1]).
-%-import(utils).
 -import(drawingTools, [drawRoads/2, drawGUI/0, printLightPair/4]).
 -import(utils, [color/1, lightsCoords/1, print/1, gotoend/0, printxy/1, printlight/1, drawHorizontalRoad/3, drawVerticalRoad/4]).
 
-%--------------------------------------------
-
-	
-%--------------------------------------------
-
-	
-%---------------------------------------
-% simulation part
 keyboardListener (MainPID) ->
   gotoend(),
   Char = io:get_chars("", 1),
@@ -72,7 +62,6 @@ counter(X,Y,Lvalue,Rvalue,MainCounter,Id,Shifter)->
 				end,
 			counter(X,Y,Lvalue+L,Rvalue+R,MainCounter-(L+R),Id,Shifter);
 
-
 		{shifterPID,PID} ->
 			counter(X,Y,Lvalue,Rvalue,MainCounter,Id,PID);
 		lcounter ->
@@ -82,7 +71,6 @@ counter(X,Y,Lvalue,Rvalue,MainCounter,Id,Shifter)->
 		quit -> ok
 
 	end.
-
 
 intersectionModelInit(Id) ->
 	CounterPid = counterInit(Id),
@@ -142,8 +130,6 @@ intersectionModelLoop(Id, VertLightPid, HorLightPid, VertGreen, GreenTimer, Coun
 	after 100 -> intersectionModelLoop(Id, VertLightPid, HorLightPid, VertGreen, GreenTimer+GTm, CounterPid,ShifterPID)
 	end.
 
-
-
 shifter(InterPIDs)->
 	receive
 		{ID,r,Delay}->
@@ -175,37 +161,26 @@ shiftcar(PID,Direction,Delay)->
 		true -> PID!newcarR
 	end.
 
-
 main() ->
   drawGUI(),
 	print({hideCursor}),
 	ListenerPID = spawn(?MODULE, keyboardListener, [self()]),
-  %ListenerPID = 0,
   IntersectionPids = [intersectionModelInit(N) || N<-[0,1,2]],
 	Shifter = spawn(?MODULE,shifter,[IntersectionPids]),
 	lists:map(fun(Z) -> Z!{shifter,Shifter}, Z end, IntersectionPids),
-  %P = [intersectionModelInit(N) || N<-[0,1,2]].
-  %io:format("~p~n",[IntersectionPids]),
 	main(ListenerPID, IntersectionPids, 1).
+
 main(ListenerPID, IntersectionPids, X) ->
-
-
-	%przełączanie świateł
 	[FirstInter, MiddleInter, LastInter] = IntersectionPids,
 	if
-		%((X - 2*100) rem (17*100) =:= 0) or ((X - 2*100 - 11*100) rem (17*100) =:= 0) ->
 		((X - 2*100) rem (23*100) =:= 0) or ((X - 2*100 - 19*100) rem (23*100) =:= 0) ->
 			FirstInter!togglelights,
 			LastInter!togglelights;
-		%(X - 2*100 - 450) rem (850) =:= 0 ->
 		((X - 2*100 - 350) rem (23*100) =:= 0) or ((X - 2*100 - 1400) rem (23*100) =:= 0)->
 			MiddleInter!togglelights;
 		true -> ok
 	end,
 
-
-
-	%generowanie samochodów na skrajnych skrzyżowaniach
 	L = rand:uniform(130),
 	R= rand:uniform(130),
 	if
@@ -226,10 +201,6 @@ main(ListenerPID, IntersectionPids, X) ->
 				print({gotoxy,1,1}),
 				print({showCursor}),
 				ok
-	  after 10 -> %co 100 cykli mija sekunda
+	  after 10 ->
 		  main(ListenerPID, IntersectionPids, X+1)
 	  end.
-
-
-   
-      
--- utils.erl
+++ utils.erl
@@ -1,32 +1,15 @@
-%%%-------------------------------------------------------------------
-%%% @author lukasz
-%%% @copyright (C) 2019, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 14. sty 2019 11:45
-%%%-------------------------------------------------------------------
 -module(utils).
--author("lukasz").
-
-%% API
 -export([color/1, lightsCoords/1, print/1, gotoend/0, printxy/1, printlight/1, drawHorizontalRoad/3, drawVerticalRoad/4]).
-
-% utils
-% \e[X;YH - przesunięcie kursora do X Y
-% \e[2J - czyszczenie ekranu
 
 lightsCoords(0) -> 6;
 lightsCoords(1) -> 31;
 lightsCoords(2) -> 56.
 
-% colors
 color(0) -> red;
 color(1) -> redamber;
 color(2) -> green;
 color(3) -> amber.
 
-% goto and clearing
 print({gotoxy,X,Y}) ->
   io:format("\e[~p;~pH",[Y,X]);
 print({clear}) ->
@@ -38,11 +21,8 @@ print({showCursor})->
 gotoend() ->
   print({gotoxy,0,16}).
 
-% color printing
 printxy({X,Y,Msg}) ->
   io:format("\e[~p;~pH~s~n",[Y,X,Msg]).
-%%printintxy({X,Y,Msg}) ->
-%%  io:format("\e[~p;~pH~p~n",[Y,X,Msg]).
 printredxy({X,Y,Msg}) ->
   io:format("\e[~p;~pH\e[91m~p~n\e[0m",[Y,X,Msg]).
 printamberxy({X,Y,Msg}) ->
@@ -50,7 +30,6 @@ printamberxy({X,Y,Msg}) ->
 printgreenxy({X,Y,Msg}) ->
   io:format("\e[~p;~pH\e[92m~p~n\e[0m",[Y,X,Msg]).
 
-% drawing lights
 printlight({X,Y,red}) ->
   printxy({X+1,Y,"_"}),
   printxy({X,Y+1,"|"}),
@@ -84,10 +63,7 @@ printlight({X,Y,green}) ->
   printxy({X,Y+3,"|"}),
   printgreenxy({X+1,Y+3,o}),
   printxy({X+2,Y+3,"|"}).
-%	printxy({X+1,Y+3,"_"}).
 
-
-% roads drawing
 drawHorizontalRoad(Xp, Dl, Y) ->
   io:format("\e[~p;~pH~*.._s~n",[Y,Xp, Dl, ""]),
   io:format("\e[~p;~pH~*..-s~n",[Y+2,Xp, Dl, ""]),
