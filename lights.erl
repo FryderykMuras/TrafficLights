6482b81b81e7c2c138420b1a7bf2c7e808315c8d
fryderykmuras <fryderyk.muras@gmail.com>
2019-01-18 01:42:47 +0100

    logs support

--- lights.erl
+++ lights.erl
@@ -1,5 +1,5 @@
 -module(lights).
--export([keyboardListener/2, lightsPair/5, main/0, intersectionModelLoop/8, shiftcar/3,shifter/1,counter/8,printer/0]).
+-export([logPrinter/1,to_timestamp/1,keyboardListener/2, lightsPair/5, main/0, intersectionModelLoop/8, shiftcar/3,shifter/1,counter/8,printer/0]).
 -import(drawingTools, [drawRoads/2, drawGUI/0, printLightPair/4]).
 -import(utils, [color/1, lightsCoords/1, print/1, gotoend/0, printxy/1, printlight/1, drawHorizontalRoad/3, drawVerticalRoad/4]).
 
@@ -189,7 +189,34 @@ printer()->
 		quit -> ok
 	end.
 
+logPrinter(Path)->
+	{{Year,Month,Day},{Hour,Min,Sec}} = erlang:localtime(),
+	Date = io_lib:format("Symulacje rozpoczeto: ~2..0w:~2..0w:~2..0w  ~2..0w-~2..0w-~4..0w\n", [Hour, Min, Sec, Day, Month, Year]),
+	receive
+		{quit,L,R}->
+			{{YearF,MonthF,DayF},{HourF,MinF,SecF}} = erlang:localtime(),
+			DateF = io_lib:format("Symulacje zakonczono: ~2..0w:~2..0w:~2..0w  ~2..0w-~2..0w-~4..0w\n", [HourF, MinF, SecF, DayF, MonthF, YearF]),
+			Duration = (to_timestamp({{YearF,MonthF,DayF},{HourF,MinF,SecF}})-to_timestamp({{Year,Month,Day},{Hour,Min,Sec}})),
+			DaysD = Duration div 86400,
+			HoursD = (Duration - DaysD*86400) div 3600,
+			MinutesD = (Duration - DaysD*86400 - HoursD*3600) div 60,
+			SecondsD = (Duration - DaysD*86400 - HoursD*3600 - MinutesD*60),
+			DurationP = io_lib:format("Symulacja trwala: ~w dni, ~w godzin, ~w minut, ~w sekund\n", [DaysD, HoursD, MinutesD, SecondsD]),
+			CountL = io_lib:format("Na lewym skrzyzowaniu wygenerowano: ~w samochodow\n", [L]),
+			CountR = io_lib:format("Na prawym skrzyzowaniu wygenerowano: ~w samochodow\n", [R]),
+			file:write_file(Path, Date++CountL++CountR++DurationP++DateF++"\n-----------------------------------------------------\n",[append])
+	end.
+
+
+to_timestamp({{Year,Month,Day},{Hours,Minutes,Seconds}}) ->
+	(calendar:datetime_to_gregorian_seconds(
+		{{Year,Month,Day},{Hours,Minutes,Seconds}}
+	) - 62167219200).
+
+
+
 main() ->
+	LogPrinterPID = spawn(?MODULE, logPrinter,["logRegister.txt"]),
 	PrinterPID = spawn(?MODULE, printer,[]),
   PrinterPID!{drawGUI},
 	PrinterPID!{print,hideCursor},
@@ -197,10 +224,10 @@ main() ->
   IntersectionPids = [intersectionModelInit(N,PrinterPID) || N<-[0,1,2]],
 	Shifter = spawn(?MODULE,shifter,[IntersectionPids]),
 	lists:map(fun(Z) -> Z!{shifter,Shifter}, Z end, IntersectionPids),
-	main(1, ListenerPID, IntersectionPids, 1,PrinterPID).
+	main(1, ListenerPID, IntersectionPids, 1,PrinterPID,0,0,LogPrinterPID).
 
 % Mode = 1 -> zielona fala w prawo, -1 -> w lewo
-main(Mode, ListenerPID, IntersectionPids, X,PrinterPID) ->
+main(Mode, ListenerPID, IntersectionPids, X,PrinterPID,LGen,RGen,LogPrinterPID) ->
 	[FirstInter, MiddleInter, LastInter] = IntersectionPids,
 %%	if
 %%		((X - 2*100) rem (23*100) =:= 0) or ((X - 2*100 - 19*100) rem (23*100) =:= 0) ->
@@ -267,23 +294,27 @@ main(Mode, ListenerPID, IntersectionPids, X,PrinterPID) ->
 
 	L = rand:uniform(ProbL),
 	R= rand:uniform(ProbR),
-	if
-		L < 2 -> FirstInter!newcarL;
-		true -> FirstInter
+	GL = if
+		L < 2 -> FirstInter!newcarL,
+			1;
+		true -> 0
 	end,
-	if
-		R < 2 -> LastInter!newcarR;
-		true -> LastInter
+	GR = if
+		R < 2 -> LastInter!newcarR,
+			1;
+		true -> 0
 	end,
 
   gotoend(),
   receive
 		{ListenerPID, "q"} ->
 			lists:map(fun(Z) -> Z!quit, Z end, IntersectionPids),
+			LogPrinterPID!{quit,LGen+GL,RGen+GR},
 			timer:sleep(1000),
 			PrinterPID!{print,clear},
 			PrinterPID!{gotoxy,1,1},
 			PrinterPID!{print,showCursor},
+			PrinterPID!quit,
 			ok;
 		{ListenerPID, "t"} ->
 			FirstInter!setred,
@@ -291,10 +322,10 @@ main(Mode, ListenerPID, IntersectionPids, X,PrinterPID) ->
 			LastInter!setred,
 			%timer:sleep(500),
 			case Mode of
-        1 ->   PrinterPID!{printxy,47,16, "<->"}, main(0, ListenerPID, IntersectionPids, -100,PrinterPID);
-        0 ->   PrinterPID!{printxy,47,16, "<--"}, main(-1, ListenerPID, IntersectionPids, -100,PrinterPID);
-        -1 ->   PrinterPID!{printxy,47,16, "-->"}, main(1, ListenerPID, IntersectionPids, -100,PrinterPID)
+        1 ->   PrinterPID!{printxy,47,16, "<->"}, main(0, ListenerPID, IntersectionPids, -100,PrinterPID,LGen+GL,RGen+GR,LogPrinterPID);
+        0 ->   PrinterPID!{printxy,47,16, "<--"}, main(-1, ListenerPID, IntersectionPids, -100,PrinterPID,LGen+GL,RGen+GR,LogPrinterPID);
+        -1 ->   PrinterPID!{printxy,47,16, "-->"}, main(1, ListenerPID, IntersectionPids, -100,PrinterPID,LGen+GL,RGen+GR,LogPrinterPID)
 			end
 	after 10 ->
-		main(Mode, ListenerPID, IntersectionPids, X+1,PrinterPID)
+		main(Mode, ListenerPID, IntersectionPids, X+1,PrinterPID,LGen+GL,RGen+GR,LogPrinterPID)
 	end.
